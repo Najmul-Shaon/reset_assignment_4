52ecb1943ef2a7b60ac7632641ead885ec6adddb
Najmul-Shaon <103214871+Najmul-Shaon@users.noreply.github.com>
2023-03-12 23:38:58 +0600

    problem two solve

--- reset_assignment_4.js
+++ reset_assignment_4.js
@@ -5,6 +5,17 @@ function mindGame(inputNumber) {
     const add10ToMultiple3 = multipleBy3 + 10;
     const divideBy2 = add10ToMultiple3 / 2;
     const finalResult = divideBy2 - 5;
-    console.log(finalResult);
+    return finalResult;
 }
 
+// problem 02
+// here is a function to identify the total string count even or odd. 
+function evenOdd(inputString) {
+    const stringLength = inputString.length;
+    if (stringLength % 2 === 0) {
+        return console.log("even");
+    }
+    else {
+        return console.log("odd");
+    }
+}
